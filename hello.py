2df1b9e523cb9e3377c71cdec97a1e46226467a6
Yang Yang <yangyang@YangdeMacBook-Pro.local>
2018-01-25 16:47:10 -0500

    B added food...

--- hello.py
+++ hello.py
@@ -2,4 +2,7 @@ print("My name is Zheyi.")
 print("I was born in July.")
 print("My name is Yang.")
 print("I was born in December.")
+print("My favorite food is seafood.")
+print("I don't know what's my favorite type of music.")
+print("My astrological sign is Sagittarius.")
 
