031d3fcee7ca38640638ab07c015792f33c9a778
Zheyi Tian <tzheyi@umich.edu>
2018-01-25 16:46:49 -0500

    A add his information.

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 print("My name is Zheyi.")
 print("I was born in July.")
+print("My favorite food is fish.")
+print("My favorite type of music is country.")
+print("My astrological sign is Cancer.")
 print("My name is Yang.")
 print("I was born in December.")
 
